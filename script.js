9a261c02bb8152f66df662123029210da72ebb26
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-04-15 15:40:24 +1000

    solution for the challange by Hiren Gondhiya

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function addStash(event) {
-  // Your code here
-}
+(function(){
+  function addStash(event) {
+    imgStash.style.top = `${event.pageY}px`;
+    imgStash.style.left = `${event.pageX}px`;
 
-// Add an event listener to the correct DOM node here - using your addStash function, and firing on click.
+    if(!imgStash.style.transform) {
+      imgStash.style.transform = 'translate(-50%)';
+    }
+    // imgStash.style.transform = 'translate(-50%)';
+    console.log(imgStash.style.transform);
+    // console.log(`${event.pageX}px;`);
+    // console.log(`${event.pageY}px;`);
+    // console.log(imgStash);
+    // console.log(event);
+  }
+  
+  // Add an event listener to the correct DOM node here - using your addStash function, and firing on click.
+  let imgCat = document.getElementById('cat-pic');
+  imgCat.addEventListener('click', addStash);
+  let imgStash = document.getElementById('stash-pic');
+})();
