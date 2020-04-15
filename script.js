a4dcc6989a75a446b6dbd7432e8cd91631faf408
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-04-15 15:46:22 +1000

    add html and js

--- script.js
+++ script.js
@@ -6,15 +6,8 @@
     if(!imgStash.style.transform) {
       imgStash.style.transform = 'translate(-50%)';
     }
-    // imgStash.style.transform = 'translate(-50%)';
     console.log(imgStash.style.transform);
-    // console.log(`${event.pageX}px;`);
-    // console.log(`${event.pageY}px;`);
-    // console.log(imgStash);
-    // console.log(event);
-  }
-  
-  // Add an event listener to the correct DOM node here - using your addStash function, and firing on click.
+  }  
   let imgCat = document.getElementById('cat-pic');
   imgCat.addEventListener('click', addStash);
   let imgStash = document.getElementById('stash-pic');
